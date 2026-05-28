e7dc4a27ff7d7abbcfa9a37ac03e5bc331d4ea5f
OindrilaD <oindriladasgupta12@gmail.com>
2026-05-28 14:15:28 +0530

    connected frontend to render backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "/api",
+  baseURL: "https://puja-backend-g4ri.onrender.com/api"
 });
 
 export default api;
